33010cff79a32316b6c76128bfdfaf6ac7fa0c9f
Sergej Dmitrenko <61592085+chichikow@users.noreply.github.com>
2020-03-29 01:15:09 +0300

    Update Longest_Common_Prefix.py

--- Longest_Common_Prefix.py
+++ Longest_Common_Prefix.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        if len(strs) == 0:
+        if len(strs) == 0:                       # if input string is empty we output empty string
             return ""
-        min_st = min(strs)                                    
-        max_st = max(strs)                                      
+        min_st = min(strs)                       # we assing the variable min_st to the first alphabetical word in out input list               
+        max_st = max(strs)                       # the last word                                   
         
-        for i in range(len(max_st)):
-            if max_st[i] != min_st[i]:
-                return max_st[:i]
-        return min_st
+        for i in range(len(max_st)):             # the number of iterations == the length of the minimum word
+            if max_st[i] != min_st[i]:           # check the words for similarity
+                return max_st[:i]                # if the character no same - output longest common prefix 
+        return min_st                            # if the words match exactly, return shorter of them
     
 arr = Solution()
 print(arr.longestCommonPrefix(['fly','flower','flame']))
