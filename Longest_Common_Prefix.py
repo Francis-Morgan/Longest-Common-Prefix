426c518dc5bf4a8749c1a9908bdc2d7d71476753
Sergej Dmitrenko <61592085+chichikow@users.noreply.github.com>
2020-04-06 21:47:46 +0300

    Update Longest_Common_Prefix.py

--- Longest_Common_Prefix.py
+++ Longest_Common_Prefix.py
@@ -9,7 +9,7 @@ class Solution(object):
         min_st = min(strs)                       # we assing the variable min_st to the first alphabetical word in out input list               
         max_st = max(strs)                       # the last word                                   
         
-        for i in range(len(max_st)):             # the number of iterations == the length of the minimum word
+        for i in range(len(min_st)):             # the number of iterations == the length of the minimum word
             if max_st[i] != min_st[i]:           # check the words for similarity
                 return max_st[:i]                # if the character no same - output longest common prefix 
         return min_st                            # if the words match exactly, return shorter of them
