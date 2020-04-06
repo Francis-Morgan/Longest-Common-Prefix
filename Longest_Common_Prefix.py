b78957c2eff5854b683efdf8e41232d5f2b06d9d
Sergej Dmitrenko <61592085+chichikow@users.noreply.github.com>
2020-04-06 23:28:31 +0300

    Update Longest_Common_Prefix.py

--- Longest_Common_Prefix.py
+++ Longest_Common_Prefix.py
@@ -1,6 +1,8 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
         """
+        function to find the longest common prefix string amongst an array of strings
+        
         :type strs: List[str]
         :rtype: str
         """
